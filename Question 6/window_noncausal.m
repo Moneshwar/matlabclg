840f21dd5eaa52251b6facec203801c5fbefe9f6
Moneshwar <76728384+Moneshwar@users.noreply.github.com>
2022-09-22 17:43:30 +0530

    Add files via upload

---
+++ Question 6/window_noncausal.m
@@ -0,0 +1,16 @@
+N=input('Enter N');
+n=(-1*(N-1)/2):(N-1)/2;
+WR=ones(1,N);
+disp(n);
+disp(WR);
+subplot(2,2,1);
+stem(n,WR);
+title('Rectangale window');
+WHn=0.5+0.5*cos(2*pi*n/(N-1));
+subplot(2,2,2);
+stem(n,WHn);
+title('Hanning window');
+WHn=0.54+0.46*cos(2*pi*n/(N-1));
+subplot(2,2,3);
+stem(n,WHn);
+title('Hamming window');
